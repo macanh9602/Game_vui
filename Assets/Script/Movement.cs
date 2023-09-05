e8273936960d750b49ee408b60c70fb442fb2035
Dai_Di_Bar <Dai_Di_Bar@DESKTOP-UK0KLJD>
2023-09-05 12:53:55 +0700

    update qvd

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -83,16 +83,28 @@ public class Movement : MonoBehaviour
             //Debug.Log(rb.velocity);
         }
     }
-    void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MovingObject")) // cham dat hoac cham vat the bay
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MovingObject"))
+        // cham dat hoac cham vat the bay
+        {
+
+            animator.SetBool("IsJump", false);
+
+        }
+    }
+    //hello
+  /*  void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("MovingObject"))
+            // cham dat hoac cham vat the bay
         {
 
             animator.SetBool("IsJump", false);
 
         }
 
-    }
+    }*/
 }
 
 
